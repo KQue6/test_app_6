8c0910fc62a39089391c615a29b5824581199d8a
KQenzé Xispoinine <fouryou42021@outlook.com>
2021-02-16 18:17:24 -0800

    Initial commit and add root route

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,58 @@
+class ApplicationController < ActionController::Base
+ 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# => Refactored Code Code
+# def hello 
+#   render html: 'Hello World!'
+# end
